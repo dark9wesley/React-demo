803ba33ae63082d13c0288d0ff9adab80986407b
pengyw <pengyw@leadingcloud.xyz>
2020-08-28 00:39:07 +0800

    实现数据初步渲染

--- cnode/src/components/articlelist.jsx
+++ cnode/src/components/articlelist.jsx
@@ -1,19 +1,58 @@
 import React,{ Component } from 'react';
 import Axios from 'axios';
-import { List } from 'antd';
+import { List , Avatar , Tag } from 'antd';
 
+const tagIcon = [
+    {
+        all:'magenta'
+    },
+    {
+        good:'volcano'
+    },
+    {
+        share:'blue'
+    },
+    {
+        ask:'gold'
+    },
+    {
+        job:'purple'
+    }
+]
 
 export default class ArticleList extends Component{
     
+    state = {
+        dataList:[]
+    }
+
     componentDidMount(){
-        Axios.get(`/topics?page=${1}&tab=${'all'}&limit=${20}`).then((data) => { console.log(data) })
+        Axios.get(
+            `/topics?page=${1}&tab=${'all'}&limit=${20}`
+            )
+            .then(
+                data => data.data.data
+                )
+                .then(
+                    data => this.setState({dataList:data})
+                )
     }
     
     render(){
+        console.log(this.state)
+        const { dataList } = this.state
         return (
-            <List>
-                
-            </List>
+            <List
+                dataSource={dataList}
+                renderItem={(item)=>{
+                    return (<List.Item key={item.id}>
+                                <List.Item.Meta 
+                                    avatar={< Avatar src={item.author.avatar_url} />}
+                                    title={ item.title }
+                                />
+                            </List.Item>)
+                }}
+            />
         )
     }
 }
